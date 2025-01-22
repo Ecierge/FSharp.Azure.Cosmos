32a2f601fe3793461a1f903b38d877f2dbd1a9e2
Andrii Chebukin <andrii@eCierge.com>
2025-01-22 04:04:45 +0400

    WIP adding build and release automation

--- src/Cosmos/Queryable.fs
+++ src/Cosmos/Queryable.fs
@@ -13,8 +13,16 @@ type IQueryable<'T> with
     member inline query.AsAsyncEnumerable<'T> ([<Optional; EnumeratorCancellation>] cancellationToken : CancellationToken) =
         query.ToFeedIterator().AsAsyncEnumerable<'T> (cancellationToken)
 
-    member inline query.MapAsyncEnumerable<'T, 'Result> (mapping : IterationState<'T> -> 'T -> 'Result, [<Optional; EnumeratorCancellation>] cancellationToken : CancellationToken) =
+    member inline query.MapAsyncEnumerable<'T, 'Result>
+        (mapping : IterationState<'T> -> 'T -> 'Result, [<Optional; EnumeratorCancellation>] cancellationToken : CancellationToken)
+        =
         query.ToFeedIterator().MapAsyncEnumerable<'T, 'Result> (mapping, cancellationToken)
 
-    member inline query.MapFoldAsyncEnumerable<'T, 'State, 'Result> (mapping : IterationState<'T> -> 'State -> 'T -> struct ('Result * 'State), state : 'State, [<Optional; EnumeratorCancellation>] cancellationToken : CancellationToken) =
+    member inline query.MapFoldAsyncEnumerable<'T, 'State, 'Result>
+        (
+            mapping : IterationState<'T> -> 'State -> 'T -> struct ('Result * 'State),
+            state : 'State,
+            [<Optional; EnumeratorCancellation>] cancellationToken : CancellationToken
+        )
+        =
         query.ToFeedIterator().MapFoldAsyncEnumerable<'T, 'State, 'Result> (mapping, state, cancellationToken)
